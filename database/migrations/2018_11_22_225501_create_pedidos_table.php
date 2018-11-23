ebe362c16ef0499b7c63a5c40a33892663e56324
DESKTOP-T4UG1Q4\milev <alexvilla@utp.edu.co>
2018-11-22 23:06:08 -0500

    mejorando codigo

--- database/migrations/2018_11_22_225501_create_pedidos_table.php
+++ database/migrations/2018_11_22_225501_create_pedidos_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePedidosTable extends Migration
 {
@@ -18,8 +18,18 @@ class CreatePedidosTable extends Migration
             $table->integer('plato_id')->unsigned();
             $table->integer('bebida_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
+
+            //relacion
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('bebida_id')->references('id')->on('bebidas')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('plato_id')->references('id')->on('platos')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
         });
     }
 
